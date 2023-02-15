cdc2f1aa211f2c75e4fb7bc4abbd51476edc47ba
bmhung <minhhung@coderpush.com>
2023-02-15 13:54:45 +0700

    feat: custom type for loginWithToken

---
+++ src_lemonade/client/action/auth.js
@@ -0,0 +1,15 @@
+import { createTemporaryClient, updateLocalStore } from '../../../src/client/action/auth';
+import cons from '../../../src/client/state/cons';
+
+export async function loginWithToken(baseUrl, token) {
+  const client = createTemporaryClient(baseUrl);
+
+  const res = await client.login('org.matrix.login.jwt', {
+    token,
+    initial_device_display_name: cons.DEVICE_DISPLAY_NAME,
+  });
+
+  const myBaseUrl = res?.well_known?.['m.homeserver']?.base_url || client.baseUrl;
+
+  updateLocalStore(res.access_token, res.device_id, res.user_id, myBaseUrl);
+}
