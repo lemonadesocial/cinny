eae9801ad01ef71c508312eed7794fa94643ef5e
bmhung <minhhung@coderpush.com>
2023-02-17 16:19:09 +0700

    feat: export navigation & cons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Client from './src/app/templates/client/Client';
 import { isAuthenticated } from './src/client/state/auth';
 import settings from './src/client/state/settings';
+import navigation from './src/client/state/navigation';
+import cons from './src/client/state/cons';
 import initMatrix from './src/client/initMatrix';
 
 import { loginWithToken } from './src_lemonade/client/action/auth';
@@ -9,6 +11,8 @@ export {
   Client,
   isAuthenticated,
   settings,
+  navigation,
+  cons,
   initMatrix,
   loginWithToken,
 }
