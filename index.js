27af451ca96e3bad8e77ad6dfd5287cc6a970877
bmhung <minhhung@coderpush.com>
2023-02-20 16:26:43 +0700

    refactor: export from

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-import Client from './src/app/templates/client/Client';
-import { isAuthenticated } from './src/client/state/auth';
-import settings from './src/client/state/settings';
-import navigation from './src/client/state/navigation';
-import cons from './src/client/state/cons';
-import initMatrix from './src/client/initMatrix';
+export { default as cons } from './src/client/state/cons';
+export { default as navigation } from './src/client/state/navigation';
+export { default as settings } from './src/client/state/settings';
+export { isAuthenticated } from './src/client/state/auth';
+export { default as initMatrix } from './src/client/initMatrix';
+export { default as Client } from './src/app/templates/client/Client';
 
-import { loginWithToken } from './src_lemonade/client/action/auth';
-
-export {
-  Client,
-  isAuthenticated,
-  settings,
-  navigation,
-  cons,
-  initMatrix,
-  loginWithToken,
-}
+export { loginWithToken } from './src_lemonade/client/action/auth';
