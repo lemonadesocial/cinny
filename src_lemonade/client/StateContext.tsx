cc3173cc09ab967f63e35640ee38ec77d02a6fdd
bmhung <minhhung@coderpush.com>
2023-03-09 21:41:05 +0700

    feat: expose actions & states in contexts

---
+++ src_lemonade/client/StateContext.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import initMatrix from '../../src/client/initMatrix';
+import { loginWithToken } from './action/auth';
+import { initHotkeys } from '../../src/client/event/hotkeys';
+import { initRoomListListener } from '../../src/client/event/roomList';
+import { isAuthenticated } from '../../src/client/state/auth';
+import { useSelectedTab } from '../../src/app/hooks/useSelectedTab';
+
+export interface CinnyStateContextType {
+  loading?: boolean,
+  selectedTab?: string,
+  inviteDirects?: Set<string>,
+}
+
+export const CinnyStateContext = React.createContext({});
+
+export function CinnyStateProvider(props) {
+  const { matrixToken } = props;
+  const [loading, changeLoading] = React.useState(true);
+  const [matrixLoggedIn, setMatrixLoggedIn] = React.useState(isAuthenticated());
+  const [inviteDirects, setInviteDirects] = React.useState(new Set<string>());
+  const [selectedTab] = useSelectedTab();
+
+  React.useEffect(() => {
+    if (!matrixToken || matrixLoggedIn) return;
+
+    (async () => {
+      await loginWithToken(process.env.MATRIX_BASE_URL, matrixToken);
+      setMatrixLoggedIn(isAuthenticated());
+    })();
+  }, [matrixToken, matrixLoggedIn]);
+
+  React.useEffect(() => {
+    if (!matrixLoggedIn) return;
+
+    initMatrix.once('init_loading_finished', () => {
+      initHotkeys();
+      initRoomListListener(initMatrix.roomList);
+      changeLoading(false);
+
+      if (initMatrix.roomList?.inviteDirects) setInviteDirects(initMatrix.roomList.inviteDirects);
+    });
+    initMatrix.init();
+  }, [matrixLoggedIn])
+
+  const values: CinnyStateContextType = React.useMemo(() => ({ 
+    loading,
+    selectedTab,
+    inviteDirects,
+  }), [loading, selectedTab, inviteDirects]);
+
+  return <CinnyStateContext.Provider {...props} value={values}/>;
+}
+
+CinnyStateProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  matrixToken: PropTypes.string,
+};
