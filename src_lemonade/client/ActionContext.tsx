2fbd49491cd4c6a994b007f32314494261fe6bf2
bmhung <minhhung@coderpush.com>
2023-03-14 19:44:12 +0700

    feat: return roomdId

--- src_lemonade/client/ActionContext.tsx
+++ src_lemonade/client/ActionContext.tsx
@@ -5,10 +5,12 @@ import { hasDMWith } from '../../src/util/matrixUtil';
 import { selectRoom } from '../../src/client/action/navigation';
 
 export interface CinnyActionContextType {
-  openDM?: (userId: string) => void,
+  openDM(userId: string): Promise<string>,
 }
 
-export const CinnyActionContext = React.createContext({});
+export const CinnyActionContext = React.createContext<CinnyActionContextType>({
+  openDM: async () => '',
+});
 
 export function CinnyActionProvider(props) {
   const openDM = async (userId: string) => {
@@ -16,14 +18,17 @@ export function CinnyActionProvider(props) {
 
     if (dmRoomId) {
       selectRoom(dmRoomId);
-      return;
+      return dmRoomId;
     }
 
     const createdRoom = await createDM(userId, false);
 
     if (createdRoom?.room_id) {
       selectRoom(createdRoom.room_id);
+      return createdRoom?.room_id;
     }
+
+    return '';
   };
 
   const values: CinnyActionContextType = React.useMemo(() => ({ 
