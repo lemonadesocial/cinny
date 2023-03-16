763090787b1c9264713af04ab637abc2add7f682
bmhung <minhhung@coderpush.com>
2023-03-16 14:06:03 +0700

    feat: do login in StateContext

--- src_lemonade/client/StateContext.tsx
+++ src_lemonade/client/StateContext.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
 import initMatrix from '../../src/client/initMatrix';
+import { loginWithToken } from './action/auth';
 import { initHotkeys } from '../../src/client/event/hotkeys';
 import { initRoomListListener } from '../../src/client/event/roomList';
+import { isAuthenticated } from '../../src/client/state/auth';
 import { useSelectedTab } from '../../src/app/hooks/useSelectedTab';
 
 export interface CinnyStateContextType {
@@ -13,17 +16,34 @@ export interface CinnyStateContextType {
 export const CinnyStateContext = React.createContext({});
 
 export function CinnyStateProvider(props) {
+  const { matrixToken } = props;
   const [loading, changeLoading] = React.useState(true);
+  const [matrixLoggedIn, setMatrixLoggedIn] = React.useState(isAuthenticated());
   const [selectedTab] = useSelectedTab();
 
   React.useEffect(() => {
+    if (!matrixToken || matrixLoggedIn) return;
+
+    (async () => {
+      try {
+        await loginWithToken(process.env.MATRIX_BASE_URL, matrixToken);
+        setMatrixLoggedIn(isAuthenticated());
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [matrixToken, matrixLoggedIn]);
+
+  React.useEffect(() => {
+    if (!matrixLoggedIn) return;
+
     initMatrix.once('init_loading_finished', () => {
       initHotkeys();
       initRoomListListener(initMatrix.roomList);
       changeLoading(false);
     });
     initMatrix.init();
-  }, []);
+  }, [matrixLoggedIn])
 
   const values: CinnyStateContextType = React.useMemo(() => ({ 
     loading,
@@ -32,3 +52,8 @@ export function CinnyStateProvider(props) {
 
   return <CinnyStateContext.Provider {...props} value={values}/>;
 }
+
+CinnyStateProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  matrixToken: PropTypes.string,
+};
