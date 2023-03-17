d95ab4b04a0e203e2e3545e6b288969c91a079b2
bmhung <minhhung@coderpush.com>
2023-03-17 15:16:52 +0700

    feat: expose joinRoom to ActionContext

--- src_lemonade/client/ActionContext.tsx
+++ src_lemonade/client/ActionContext.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
+import { Room } from 'matrix-js-sdk';
 
-import { createDM } from '../../src/client/action/room';
+import initMatrix from '../../src/client/initMatrix';
+import { createDM, join } from '../../src/client/action/room';
 import { hasDMWith } from '../../src/util/matrixUtil';
 import { selectRoom } from '../../src/client/action/navigation';
 
 export interface CinnyActionContextType {
   openDM(userId: string): Promise<string>,
+  getRoom(roomId: string): Promise<Room | null>,
+  joinRoom(roomId: string): Promise<string>,
 }
 
 export const CinnyActionContext = React.createContext<CinnyActionContextType>({
   openDM: async () => '',
+  getRoom: async () => null,
+  joinRoom: async () => '',
 });
 
 export function CinnyActionProvider(props) {
-  const openDM = async (userId: string) => {
+  async function openDM(userId: string) {
     const dmRoomId = hasDMWith(userId);
 
     if (dmRoomId) {
@@ -31,8 +37,24 @@ export function CinnyActionProvider(props) {
     return '';
   };
 
+  async function getRoom(roomId: string) {
+    if (!initMatrix.matrixClient) return null;
+
+    const myRoom = initMatrix.matrixClient.getRoom(roomId);
+    if (myRoom === null) return null;
+
+    return myRoom;
+  }
+
+  async function joinRoom(roomId: string, isDM = true) {
+    const result = await join(roomId, isDM);
+    return result;
+  }
+
   const values: CinnyActionContextType = React.useMemo(() => ({ 
     openDM,
+    getRoom,
+    joinRoom,
   }), []);
 
   return <CinnyActionContext.Provider {...props} value={values}/>;
