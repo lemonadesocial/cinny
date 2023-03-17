c5e3185817ddf473426fa8de61e1618f3b90e967
bmhung <minhhung@coderpush.com>
2023-03-17 15:17:24 +0700

    feat: create BasicRoomView to show in sidepane

---
+++ src_lemonade/client/templates/BasicRoomView.jsx
@@ -0,0 +1,53 @@
+// This copied from src/app/organisms/room/RoomView.jsx
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import EventEmitter from 'events';
+
+import RoomViewContent from '../../../src/app/organisms/room/RoomViewContent';
+import RoomViewFloating from '../../../src/app/organisms/room/RoomViewFloating';
+import BasicRoomViewInput from './BasicRoomViewInput';
+
+import '../../../src/app/organisms/room/RoomView.scss';
+
+const viewEvent = new EventEmitter();
+
+function BasicRoomView({ roomTimeline, eventId }) {
+  const roomViewRef = useRef(null);
+
+  const { roomId } = roomTimeline;
+
+  return (
+    <div className="room-view" ref={roomViewRef}>
+      <div className="room-view__content-wrapper">
+        <div className="room-view__scrollable">
+          <RoomViewContent
+            eventId={eventId}
+            roomTimeline={roomTimeline}
+          />
+          <RoomViewFloating
+            roomId={roomId}
+            roomTimeline={roomTimeline}
+          />
+        </div>
+        <div className="room-view__sticky">
+          <BasicRoomViewInput
+            roomId={roomId}
+            roomTimeline={roomTimeline}
+            viewEvent={viewEvent}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+BasicRoomView.defaultProps = {
+  eventId: null,
+};
+BasicRoomView.propTypes = {
+  roomTimeline: PropTypes.shape({ roomId: PropTypes.string }).isRequired,
+  eventId: PropTypes.string,
+};
+
+export default BasicRoomView;
