f16a347680f2368818825efdb318295217506a3a
bmhung <minhhung@coderpush.com>
2023-06-17 00:48:17 +0700

    feat: build lib

--- viteSvgLoader.ts
+++ viteSvgLoader.ts
@@ -5,12 +5,13 @@ import fs from 'fs';
 // TODO: remove this once https://github.com/vitejs/vite/pull/2909 gets merged
 export const svgLoader = (): Plugin => ({
   name: 'vite-svg-patch-plugin',
-  transform: (code, id) => {
-    if (id.endsWith('.svg')) {
-      const extractedSvg = fs.readFileSync(id, 'utf8');
-      const datauri = svgToMiniDataURI.toSrcset(extractedSvg);
-      return `export default "${datauri}"`;
+  load: (id) => {
+    if (!id.endsWith('.svg')) {
+      return null;
     }
-    return code;
+
+    const extractedSvg = fs.readFileSync(id, 'utf8');
+    const datauri = svgToMiniDataURI.toSrcset(extractedSvg);
+    return `export default "${datauri}"`;
   },
 });
