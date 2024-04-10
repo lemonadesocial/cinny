2e10958a4783c867e9bfe062ccaf926a71140002
bmhung <bui.m.hung@gmail.com>
2024-04-10 14:15:46 +0700

    feat: update welcome

--- src/app/organisms/welcome/Welcome.jsx
+++ src/app/organisms/welcome/Welcome.jsx
@@ -3,15 +3,15 @@ import './Welcome.scss';
 
 import Text from '../../atoms/text/Text';
 
-import CinnySvg from '../../../../public/res/svg/cinny.svg';
+import ChatSvg from '../../../../public/res/svg/chat.svg';
 
 function Welcome() {
   return (
     <div className="app-welcome flex--center">
       <div>
-        <img className="app-welcome__logo noselect" src={CinnySvg} alt="Cinny logo" />
-        <Text className="app-welcome__heading" variant="h1" weight="medium" primary>Welcome to Cinny</Text>
-        <Text className="app-welcome__subheading" variant="s1">Yet another matrix client</Text>
+        <img className="app-welcome__logo noselect" src={ChatSvg} alt="Townhall" />
+        <Text className="app-welcome__heading" variant="h1" weight="medium" primary>Lemonade Townhall</Text>
+        <Text className="app-welcome__subheading" variant="s1">Powered by [matrix]</Text>
       </div>
     </div>
   );
